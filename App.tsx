6402eb3cbc9c7c7f92d398b3e8b531d98f0e98be
TSerup <au575786@post.au.dk>
2020-10-07 19:06:10 +0200

    second commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
   return (
