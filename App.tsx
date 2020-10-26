a003d1eeccb6da15f2e0f9ef0b728a278f7e4a20
TSerup <au575786@post.au.dk>
2020-10-26 11:31:39 +0100

    final commit

--- App.tsx
+++ App.tsx
@@ -1,35 +1,112 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View } from 'react-native';
-import { Camera } from 'expo-camera';
+import React, { useState, useRef, useEffect } from "react";
+import { View, Text, TouchableOpacity, SafeAreaView} from "react-native";
+import { Camera } from "expo-camera";
+import { FontAwesome, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 import styles from './styles';
-import CamButtons from './src/CamButtons';
+
 
 export default function App() {
-  const [Permission, setPermission] = useState("");
-  const [type, setType] = useState(Camera.Constants.Type.back);
-  
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [isPreview, setIsPreview] = useState(false);
+  const cameraRef = useRef();
+
+  // Start by getting permission to access camera. 
+  // On IOS it's also needed to get permission to access cameraroll
   useEffect(() => {
-    (async () => { 
-      let { status } = await Camera.requestPermissionsAsync();
-      if(status !== 'granted')
-        setPermission('Permission to access camera was denied');
-    })();
+    (async () => {
+      const permission = await Camera.requestPermissionsAsync();
+      setHasPermission(permission.status === "granted");
+  })();
   }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text style={styles.text}>No access to camera</Text>;
+  }
+
+  // Function for taking pictures and setting preview. 
+  // Pictures are saved in cache, and to save picture use Expo.FileSystem.copyAsync
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 0.5, base64: true, skipProcessing: true };
+      const { uri } = await cameraRef.current.takePictureAsync(options);
+      
+      if (uri) {
+        await cameraRef.current.pausePreview();
+        setIsPreview(true);
+        console.log("picture source", uri);
+      }
+    }
+  };
   
-  // Toggles the camera between back and front
-  function cameraType() {
-    setType(
-      type === Camera.Constants.Type.back
+  // Function for accessing the picture folder
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      // Change 'Images' to 'All' and you will also have videos available
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+  };
+
+// Function for toggling the camera between back and front
+  const switchCamera = () => {
+    if (isPreview) {
+      return;
+    }
+    setCameraType((prevCameraType) =>
+      prevCameraType === Camera.Constants.Type.back
         ? Camera.Constants.Type.front
         : Camera.Constants.Type.back
     );
-  }
-  return (
-    <View style={styles.fullScreen}>
-      <Text style={styles.text}>{Permission}</Text>
-      <Camera style={styles.fullScreen} type={type}>
-        <CamButtons onPressSwitch={cameraType}/> 
-      </Camera>
+  };
+
+  // Function for returning from preview
+  const cancelPreview = async () => {
+    await cameraRef.current.resumePreview();
+    setIsPreview(false);
+  };
+
+  // When in preview mode show button to return to normal mode
+  const renderCancelPreviewButton = () => (
+    <TouchableOpacity onPress={cancelPreview} style={styles.closeButtonArea}>
+      <Ionicons name="md-close-circle" style={styles.closeButton}></Ionicons>
+    </TouchableOpacity>
+  );
+
+  // Normal mode
+  const renderCaptureControl = () => (
+    <View style={styles.view}>
+      <TouchableOpacity onPress={switchCamera} style={styles.touchable}>
+        <MaterialCommunityIcons name="camera-switch" style={styles.icons}/>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={takePicture} style={styles.touchable}>
+        <FontAwesome name="camera" style={styles.icons}/>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.touchable} onPress={pickImage}>
+        <Ionicons name="ios-photos" style={styles.icons}/>
+      </TouchableOpacity>
     </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Camera
+        ref={cameraRef}
+        style={styles.container}
+        type={cameraType}
+        flashMode={Camera.Constants.FlashMode.on}
+        onMountError={(error) => {console.log("camera error", error);
+        }}/>
+      <View style={styles.container}>
+        {isPreview && renderCancelPreviewButton()}
+        {!isPreview && renderCaptureControl()}
+      </View>
+    </SafeAreaView>
   );
 }
--- styles.tsx
+++ styles.tsx
@@ -1,28 +1,60 @@
 import { StyleSheet } from 'react-native';
 import { Dimensions } from 'react-native';
 
-const windowWidth = Dimensions.get('window').width;
-const windowHeight = Dimensions.get('window').height;
+const {height, width} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-    view: {
-        flex: 1,
-        backgroundColor: 'transparent',
-        flexDirection: 'row',
+    container: {
+      ...StyleSheet.absoluteFillObject, 
+      width: width+180
     },
-    text: { 
-        fontSize: 18, 
-        marginBottom: 10, 
-        color: 'white' 
+    closeButtonArea: {
+      position: "absolute",
+      top: 40,
+      left: 20,
+      height: 80,
+      width: 80,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "transparent",
+    },
+    closeButton: {
+      height: 60,
+      width: 60,
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: 50,
+      color: '#fff',
+      opacity: 0.5
+    },
+    recordIndicatorContainer: {
+      flexDirection: "row",
+      position: "absolute",
+      top: 25,
+      alignSelf: "center",
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: "transparent",
+      opacity: 0.7,
+    },
+    text: {
+      color: "#fff",
     },
     touchable: {
-        flex: 0.1,
-        alignSelf: 'flex-end',
-        alignItems: 'center',
-      },
-    fullScreen: {
-        width: windowWidth+180,
-        height: windowHeight,
-      },
+      alignSelf: 'flex-end',
+      alignItems: 'center',
+      backgroundColor: 'transparent',
+    },
+    icons: {
+      color: "#fff",
+      fontSize: 40
+    },
+    view: {
+      width: width+35,
+      height: height,
+      flexDirection:"row",
+      justifyContent:"space-around",
+       margin:-15,
+    },
   });
   export default styles;
